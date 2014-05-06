0a5b0b54f4a3f8b4921858e00ee834429f2210e1
Patrick DELVENNE <wintzx@no-log.org>
2014-05-06 15:45:22 +0200

    Remove test file

--- xx.sh
+++
@@ -1,9 +0,0 @@
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-
-touch README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/wintzx/i2cRaspi.git
-git push -u origin master
